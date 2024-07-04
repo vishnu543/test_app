9cecbe0e68d4c505e0e427875bd10a6b11c1f567
vishnu543 <vishnuknl543@gmail.com>
2024-07-04 15:54:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const router = require('express').Router();
 
 
 const userRoutes = require('./routes/user');
